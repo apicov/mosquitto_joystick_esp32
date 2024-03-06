e8b3e372a82842f57305db9ceda90ac435187cfb
apicov <a.pico@posteo.de>
2024-03-06 19:45:31 +0100

    Add functions to read joystick pins

--- mqtt_joystick_esp32/src/main.cpp
+++ mqtt_joystick_esp32/src/main.cpp
@@ -4,6 +4,15 @@
 
 #include "private_data.h"
 
+int LED_BUILTIN = 2;
+
+#define RED_BUTTON 34
+#define UP 35
+#define DOWN 32
+#define LEFT 33
+#define RIGHT 25
+
+#define N_PINS 5
 /*
 #define SSID "ssid"
 #define PASSWORD "password"
@@ -17,25 +26,79 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const char* mqtt_topic = "xcarve_ctl/joystick/cmd";
-const char* joystick_cmds[] = {"Key.up","Key.down", "Key.right", "Key.left"};
+const char* joystick_cmds[] = {"Button.red", "Key.up", "Key.down", "Key.left", "Key.right"};
 int cont  = 0;
+
+
+const uint8_t joystick_pins[N_PINS] = {RED_BUTTON, UP, DOWN, LEFT, RIGHT};
+uint8_t joystick_status[N_PINS] = {0,0,0,0,0};
+void read_joystick(uint8_t* status_array);
+void print_array(uint8_t* array);
+
+
+void mqtt_connect();
+void initWiFi();
 
 void setup() {
 
   Serial.begin(9600);
 
+  pinMode (LED_BUILTIN, OUTPUT);
+
+  pinMode(RED_BUTTON, INPUT );
+  pinMode(UP, INPUT );
+  pinMode(DOWN, INPUT );
+  pinMode(LEFT, INPUT );
+  pinMode(RIGHT, INPUT );
+
+
   // Connect to Wi-Fi
-  WiFi.begin(SSID, PASSWORD);
+  /*WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       Serial.println("Connecting to WiFi..");
   }
-  Serial.println("Connected to the WiFi network");
+  Serial.println("Connected to the WiFi network");*/
 
+  //initWiFi();
+  Serial.println("Connected to the WiFi network");
 
   // Set the MQTT server to the client
   client.setServer(mqtt_broker, mqtt_port);
   
+  mqtt_connect();
+}
+
+void loop() {
+
+  //connect to network in case there is no conection
+    if (WiFi.status() != WL_CONNECTED){
+      initWiFi();
+      Serial.print("wifi connected.");
+    }
+  
+  if (!client.connected()) {
+    mqtt_connect();
+  }
+
+ read_joystick(joystick_status);
+ //print_array(joystick_status);
+
+ //check joystick status, publish first one found in array
+ for(int i=0; i<N_PINS; i++){
+  if (joystick_status[i] == 1){
+    // Publish a message
+    client.publish(mqtt_topic, joystick_cmds[i]);
+    Serial.print(joystick_cmds[i]);
+    break;
+  }
+ }
+ delay(100);
+
+}
+
+
+void mqtt_connect() {
   // Connect to MQTT Broker
   while (!client.connected()) {
       Serial.println("Connecting to MQTT Broker...");
@@ -49,15 +112,33 @@ void setup() {
   }
 }
 
-void loop() {
-  if (cont  < 4){
-    // Publish a message
-    client.publish(mqtt_topic, joystick_cmds[cont]);
-    cont++;
-  }
-  else{
-    cont = 0;
+
+void read_joystick(uint8_t* status_array){
+  for(int i=0; i<N_PINS; i++ ){
+    status_array[i] = !digitalRead(joystick_pins[i]);
   }
 
-  delay(5000);
+}
+
+void print_array(uint8_t* array){
+  for(int i=0; i<N_PINS; i++){
+    Serial.print(array[i]);
+    Serial.print(" ");
+  }
+  Serial.println("");
+}
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print('.');
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+  Serial.println(WiFi.localIP());
 }
