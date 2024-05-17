b347614cc5977edaaf3c1e04949da87985310f42
pico <a.pico@posteo.de>
2024-05-17 16:09:26 +0200

    add mqtt subscriber for commands to enable and disable joystick

--- mqtt_joystick_esp32/src/main.cpp
+++ mqtt_joystick_esp32/src/main.cpp
@@ -27,13 +27,16 @@ const int mqtt_port = port_n;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void mqtt_subscriber_callback(char* topic, byte* payload, unsigned int length);
+
 const char* mqtt_topic = "xcarve_ctl/joystick/cmd";
 const char* joystick_cmds[] = {"Button.red", "Key.up", "Key.down", "Key.left", "Key.right","Key.stop"};
 int cont  = 0;
 
 //keeps index of last status 
 int previous_status_idx = -1;
-
+//1, jostick is enabled to send commands, 0 is disabled
+int joystick_enabled_flg = 1;
 
 const uint8_t joystick_pins[N_PINS] = {RED_BUTTON, UP, DOWN, LEFT, RIGHT};
 uint8_t joystick_status[STATUS_ARRAY_LENGTH] = {0,0,0,0,0,0}; //stop command (last position in status array) when all pins are zeros
@@ -61,8 +64,10 @@ void setup() {
 
   // Set the MQTT server to the client
   client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(mqtt_subscriber_callback);
   
   mqtt_connect();
+  client.subscribe("joystick/enable");
 }
 
 void loop() {
@@ -76,34 +81,40 @@ void loop() {
   //connect to mosquitto server if not connected
   if (!client.connected()) {
     mqtt_connect();
+    client.subscribe("joystick/enable");
   }
 
-  read_joystick(joystick_status);
-  //print_status_array(joystick_status);
+  //check for incomming messages
+  client.loop();
 
-  //check joystick status, publish first number 1 found in array
+  //only send commands when joystick is enabled
+  if (joystick_enabled_flg){
 
-  //check if red button was pressed, only sends message if new command is different from the last sent command
-  if (joystick_status[0] == 1 && previous_status_idx != 0){
-    //publish message
-    client.publish(mqtt_topic, joystick_cmds[0]);
-    delay(5000); //wait a while for avoiding  mechanical bouncing of button
-    previous_status_idx = 0;
-  }
+      read_joystick(joystick_status);
+      //print_status_array(joystick_status);
 
-  else { //check other status flags
-    for(int i=1; i<STATUS_ARRAY_LENGTH; i++){
-      if (joystick_status[i] == 1 && previous_status_idx != i){
-        // Publish a message only if new command is different from the last sent command
-        client.publish(mqtt_topic, joystick_cmds[i]);
-        Serial.println(joystick_cmds[i]);
-        previous_status_idx = i;
-        break;
+      //check joystick status, publish first number 1 found in array
+      //check if red button was pressed, only sends message if new command is different from the last sent command
+      if (joystick_status[0] == 1 && previous_status_idx != 0){
+        //publish message
+        client.publish(mqtt_topic, joystick_cmds[0]);
+        delay(5000); //wait a while for avoiding  mechanical bouncing of button
+        previous_status_idx = 0;
       }
-    }
-
+      else { //check other status flags
+        for(int i=1; i<STATUS_ARRAY_LENGTH; i++){
+          if (joystick_status[i] == 1 && previous_status_idx != i){
+            // Publish a message only if new command is different from the last sent command
+            client.publish(mqtt_topic, joystick_cmds[i]);
+            Serial.println(joystick_cmds[i]);
+            previous_status_idx = i;
+            break;
+          }
+        }
+      }
 
   }
+
  delay(70);
 
 }
@@ -120,6 +131,21 @@ void mqtt_connect() {
           Serial.print(client.state());
           delay(5000);
       }
+  }
+}
+
+
+void mqtt_subscriber_callback(char* topic, byte* payload, unsigned int length){
+  //receives ascii code of 1(49)enable and 0(48)disable joystick
+  joystick_enabled_flg = (*payload)==48?0:1;
+  if (joystick_enabled_flg){
+    previous_status_idx = -1; //ensures current cmd will be sent
+    Serial.println("joystick enabled ");
+  }
+  else{
+    //send stop xcarve command  when disabled
+    client.publish(mqtt_topic, joystick_cmds[STATUS_ARRAY_LENGTH-1]);
+    Serial.println("joystick disabled ");
   }
 }
 
